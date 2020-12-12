ceac6bc7b5f8992f4277289a8ad8627ce14fd5b1
jespadas <julio.espadas@gmail.com>
2020-12-12 13:18:04 +0100

    api keys added

--- src/db.js
+++ src/db.js
@@ -1,15 +1,15 @@
-import { firebase } from "firebase/app";
-import "firebase/database";
+import firebase from 'firebase'
+import 'firebase/database'
 
-const conf = {
-    // API KEYS
-    apiKey: "AIzaSyB7W7ABcnK22whS-ahpFcPJnWhTuDqCfgs",
-    authDomain: "vue-chat-1f1dd.firebaseapp.com",
-    projectId: "vue-chat-1f1dd",
-    storageBucket: "vue-chat-1f1dd.appspot.com",
-    messagingSenderId: "803883661315",
-    appId: "1:803883661315:web:14e7b3faf8241d47a83636"
+const config = {
+  // API KEYS
+  apiKey: 'AIzaSyB7W7ABcnK22whS-ahpFcPJnWhTuDqCfgs',
+  authDomain: 'vue-chat-1f1dd.firebaseapp.com',
+  projectId: 'vue-chat-1f1dd',
+  storageBucket: 'vue-chat-1f1dd.appspot.com',
+  messagingSenderId: '803883661315',
+  appId: '1:803883661315:web:14e7b3faf8241d47a83636'
 }
 
-const db = firebase.initializeApp(config);
-export default db;
+const db = firebase.initializeApp(config)
+export default db
